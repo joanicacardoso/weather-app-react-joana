49faaac295c203331c07b884180bc0c9ba756f67
joanicacardoso <joana.cardoso.reis@gmail.com>
2020-09-01 11:06:17 +0100

    Created new Weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         Weather App
+        <Weather />
         <footer>
           <a
             href="https://github.com/joanicacardoso/weather-app-react-joana"
