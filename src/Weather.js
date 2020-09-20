2110a6de9f53a32cc014b2dc38608d205668909a
joanicacardoso <joana.cardoso.reis@gmail.com>
2020-09-20 14:21:01 +0100

    Created new component WeatherInfo and set up the search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,56 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormatedDate from "./FormatedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
-import img from "./img/01d.svg";
 
 export default function Weather(props) {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
   function handleResponse(response) {
     setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: "./img/01d.svg",
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
-    setReady(true);
   }
 
-  if (ready) {
+  function search() {
+    const apiKey = "7a8048c0d2acbeeb84d126f5c346c59f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefaut();
+    //search for a city
+    search();
+  }
+
+  function handleCityChange(event) {
+    //update city
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <div className=" row justify-content-between no-gutters">
           <div className="col-9">
-            <form className="form-inline">
+            <form onSubmit={handleSubmit} className="form-inline">
               <div className="form-group">
                 <input
                   type="text"
                   className="form-control form-control-sm input-sm"
                   placeholder="Enter a city"
                   autoComplete="off"
+                  onChange={handleCityChange}
                 />
                 <input
                   type="submit"
@@ -41,82 +60,12 @@ export default function Weather(props) {
               </div>
             </form>
           </div>
-          <div className="Units col-3">
-            <a href="/">ºC</a> | <a href="/">ºF</a>
-          </div>
         </div>
-        <div className="row no-gutter">
-          <div className="col-10">
-            Last updated:
-            <FormatedDate date={weatherData.date} />
-          </div>
-          <div className="col-2">
-            <a href="/">O</a>
-          </div>
-        </div>
-        <div className="Current">
-          <img
-            src={weatherData.icon}
-            alt="Sunny"
-            height="120px"
-            width="120px"
-          />
-          <h1>{Math.round(weatherData.temperature)}º</h1>
-          <h1>{weatherData.city}</h1>
-          <p className="text-capitalize">{weatherData.description}</p>
-        </div>
-        <div className="Conditions">
-          <p>
-            Humidity: {weatherData.humidity}% <br />
-            Wind: {weatherData.wind}km/h
-          </p>
-        </div>
-        <div className="Forecast">
-          <div className="row justify-content-between no-gutters">
-            <div className="col-2">
-              <p>10:00</p>
-              <img src={img} alt="Sunny" height="30px" width="30px" />
-              <p>
-                <strong>25</strong> / 12
-              </p>
-            </div>
-            <div className="col-2">
-              <p>10:00</p>
-              <img src={img} alt="Sunny" height="30px" width="30px" />
-              <p>
-                <strong>25</strong> / 12
-              </p>
-            </div>
-            <div className="col-2">
-              <p>10:00</p>
-              <img src={img} alt="Sunny" height="30px" width="30px" />
-              <p>
-                <strong>25</strong> / 12
-              </p>
-            </div>
-            <div className="col-2">
-              <p>10:00</p>
-              <img src={img} alt="Sunny" height="30px" width="30px" />
-              <p>
-                <strong>25</strong> / 12
-              </p>
-            </div>
-            <div className="col-2">
-              <p>10:00</p>
-              <img src={img} alt="Sunny" height="30px" width="30px" />
-              <p>
-                <strong>25</strong> / 12
-              </p>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "48f466c2eeef74f8d4b3c29e67806457";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,78 @@
+import React from "react";
+import img from "./img/01d.svg";
+import FormatedDate from "./FormatedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="Units col-3">
+        <a href="/">ºC</a> | <a href="/">ºF</a>
+      </div>
+      <div className="row no-gutter">
+        <div className="col-10">
+          Last updated:
+          <FormatedDate date={props.data.date} />
+        </div>
+        <div className="col-2">
+          <a href="/">O</a>
+        </div>
+      </div>
+      <div className="Current">
+        <img src={props.data.icon} alt="Sunny" height="120px" width="120px" />
+        <h1>{Math.round(props.data.temperature)}º</h1>
+        <h1>{props.data.city}</h1>
+        <p className="text-capitalize">{props.data.description}</p>
+      </div>
+      <div className="Conditions">
+        <p>
+          Humidity: {props.data.humidity}% <br />
+          Wind: {props.data.wind}km/h
+        </p>
+      </div>
+      <div className="Forecast">
+        <div className="row justify-content-between no-gutters">
+          <div className="col-2">
+            <p>10:00</p>
+            <img
+              src={props.data.icon}
+              alt={props.data.description}
+              height="30px"
+              width="30px"
+            />
+            <p>
+              <strong>25</strong> / 12
+            </p>
+          </div>
+          <div className="col-2">
+            <p>10:00</p>
+            <img src={img} alt="Sunny" height="30px" width="30px" />
+            <p>
+              <strong>25</strong> / 12
+            </p>
+          </div>
+          <div className="col-2">
+            <p>10:00</p>
+            <img src={img} alt="Sunny" height="30px" width="30px" />
+            <p>
+              <strong>25</strong> / 12
+            </p>
+          </div>
+          <div className="col-2">
+            <p>10:00</p>
+            <img src={img} alt="Sunny" height="30px" width="30px" />
+            <p>
+              <strong>25</strong> / 12
+            </p>
+          </div>
+          <div className="col-2">
+            <p>10:00</p>
+            <img src={img} alt="Sunny" height="30px" width="30px" />
+            <p>
+              <strong>25</strong> / 12
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
